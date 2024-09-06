83f01d09903d5bb37cac3aa677fe63082b4dfec2
Julienne H <juliennehattabi@gmail.com>
2024-09-06 11:25:59 +0200

    fix : Fixed index overflow issue in Slider component to avoid blank slide

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,11 +7,14 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  const byDateDesc = data?.focus.sort(
+    (evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1) // a modifier pour ordre des événements
   );
   const nextCard = () => {
-    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
+    setTimeout(
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      5000
+    );
   };
   useEffect(() => {
     nextCard();
